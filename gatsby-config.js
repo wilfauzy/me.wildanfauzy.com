30ffa504e6a8408671ce394970ad607f1cc94a8d
wildan <me@wildanfauzy.com>
2021-01-14 05:23:35 +0700

    akaja

---
+++ gatsby-config.js
@@ -0,0 +1,66 @@
+require(`dotenv`).config({
+  path: `.env`,
+})
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+module.exports = {
+  siteMetadata: {
+        siteTitle: `Wildan Fauzy`,
+    siteTitleAlt: `Wildan Fauzy`,
+    siteHeadline: `Wildan Fauzy`,
+    siteUrl: `https://a.idan.my.id`,
+    siteDescription: `my name wildan fauzy just call me idan or aidan`,
+    siteLanguage: `en`,
+    siteImage: `/banner.jpg`,
+    author: `@wilfauzy`,
+    siteTitleAlt: `Wildan Fauzy`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Aidan`,
+        short_name: `idan`,
+        description: `my name wildan fauzy just call me idan or aidan`,
+        start_url: `/`,
+        background_color: `#141821`,
+        theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean),
+}
