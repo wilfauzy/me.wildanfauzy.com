931472d08c7dcc90e26a458309f9571be56d61de
wilfauzy <me@wildanfauzy.com>
2022-01-05 04:05:30 +0700

    pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
         siteTitle: `Wildan Fauzy`,
     siteTitleAlt: `Wildan Fauzy`,
     siteHeadline: `Wildan Fauzy`,
-    siteUrl: `https://wildanfauzy.com`,
+    siteUrl: `https://wilfauzy.com`,
     siteDescription: `my name wildan fauzy just call me idan or aidan`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-react-helmet-canonical-urls`,
       options: {
-        siteUrl: `https://wildanfauzy.com`,
+        siteUrl: `https://wilfauzy.com`,
       },
     },
     {
